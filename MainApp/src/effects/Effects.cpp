e28d3954f7a1d0eb9412ac5262b7d0a5eed52a64
roxlu <diederick@apollomedia.nl>
2012-03-10 19:47:34 +0100

    fixing redness

--- MainApp/src/effects/Effects.cpp
+++ MainApp/src/effects/Effects.cpp
@@ -95,7 +95,7 @@ void Effects::setup(int w, int h) {
 void Effects::beginGrabPixels() {
 	ofEnableNormalizedTexCoords();
 	glBindFramebuffer(GL_FRAMEBUFFER, fbo_handle);
-	glClearColor(1,0,0,1);
+	glClearColor(0,0,0,1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glViewport(0,0,width, height);
 }
