b40346eaac810d757315f1b6952c05250c067475
Joel Gethin Lewis <me@joelgethinlewis.com>
2012-03-19 20:18:13 +0000

    no tinting in texture

--- MainApp/src/scenes/TextureScene/TextureScene.cpp
+++ MainApp/src/scenes/TextureScene/TextureScene.cpp
@@ -163,6 +163,7 @@ void TextureScene::draw() {
         float r = it->getRadius();
         
         ofSetColor(it->colour);
+        ofSetColor(255,255,255);
         
         it->tex->bind();
         glBegin(GL_QUADS);
